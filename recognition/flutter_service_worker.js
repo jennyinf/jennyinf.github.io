944e8592aab972908208921c7bf9b1a73d95dfda
Jenny Horne <jenny.horne@infonote.com>
2023-12-05 16:14:08 +0000

    try again -use js not html

--- recognition/flutter_service_worker.js
+++ recognition/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "24d9e4d840cc512cbd2bfa604da2108e",
-"index.html": "7e920d1a6b0944ba47ccd8228bd35b88",
-"/": "7e920d1a6b0944ba47ccd8228bd35b88",
-"main.dart.js": "9b723cbdb787b2682721f57b2c163e82",
+"index.html": "df15abf59f48fc64e7a15a2b235eaac1",
+"/": "df15abf59f48fc64e7a15a2b235eaac1",
+"main.dart.js": "6cfbb73fd6936db1874ee2823edca861",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
