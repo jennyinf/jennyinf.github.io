e879c4050b2750caa9c19a7ff51af981e716bc44
Jenny Horne <jenny.horne@infonote.com>
2024-06-27 16:33:19 +0100

    recognition build

--- recognition/flutter_bootstrap.js
+++ recognition/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2502841966"
+    serviceWorkerVersion: "255429795"
   }
 });
